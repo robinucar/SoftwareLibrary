3ccaa36b423f7b5199fac8f4263451c4eaa17aea
robin ucar <mehmet.ucar@imediapro.co.uk>
2023-02-07 23:27:35 +0300

    connect with db

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log("Hello World");
+//create express
+const express = require("express");
+
+// create .env for security password
+const dotenv = require("dotenv");
+
+// create mongoose
+const mongoose = require("mongoose");
+
+//configure dotenv
+
+dotenv.config();
+
+//create app with express
+
+const app = express();
+
+const mongoUrl = process.env.MONGODB_URL;
+
+const PORT = 5000;
+
+
+mongoose.set("strictQuery", false)
+// connect with DB
+mongoose
+  .connect(mongoUrl, {
+    
+  })
+  .then(() => console.log("Conmnected to DB"))
+  .then(() => {
+    app.listen(PORT);
+  })
+  .catch((err) => console.error(err));
